664ed3d33005fddb07751c8be4ac19069eaa02d0
waleed-durrani <wallikhan76@gmail.com>
2019-11-30 14:29:17 +0500

    Updates

---
+++ precache-manifest.514c8fbea1eb1140f4a166ea17d85d0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b3400584d1c9b6336d8c879aafa9d6f",
+    "url": "/react-first/index.html"
+  },
+  {
+    "revision": "008c71eace2fb768c5c0",
+    "url": "/react-first/static/css/main.a4984997.chunk.css"
+  },
+  {
+    "revision": "6fea7dec174f8202d156",
+    "url": "/react-first/static/js/2.4734f653.chunk.js"
+  },
+  {
+    "revision": "008c71eace2fb768c5c0",
+    "url": "/react-first/static/js/main.ece91fff.chunk.js"
+  },
+  {
+    "revision": "a0836dd239d9ebd0af62",
+    "url": "/react-first/static/js/runtime-main.f0869d81.js"
+  }
+]);
